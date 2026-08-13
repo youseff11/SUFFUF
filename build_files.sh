5d15974bd90e20e38215245f0988a94475af22a3
Yousef Osama <yo5739240@gmail.com>
2026-08-13 22:38:04 +0300

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
 echo "BUILD START"
+
+# تثبيت المكتبات وتحديث pip
+python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+# تشغيل collectstatic
 python3 manage.py collectstatic --noinput --clear
+
 echo "BUILD END"
