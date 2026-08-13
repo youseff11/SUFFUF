7b32b415d9f826ed2f543c916b2fb95cac4048de
Yousef Osama <yo5739240@gmail.com>
2026-08-13 22:56:08 +0300

    Switch to modern Vercel serverless deployment

--- Core/settings.py
+++ Core/settings.py
@@ -46,13 +46,17 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Core.urls'
 
+# ------------------------------------------------------------------
+# Templates Configuration (Fixed for Vercel Serverless Import Issue)
+# ------------------------------------------------------------------
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
