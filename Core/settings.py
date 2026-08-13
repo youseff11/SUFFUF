87426a32c203705225fc0fe76f7c5813e0168203
Yousef Osama <yo5739240@gmail.com>
2026-08-13 23:05:06 +0300

    Switch to modern Vercel serverless deployment

--- Core/settings.py
+++ Core/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,9 +115,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 
-# إضافة هذا السطر لحل مشكلة collectstatic وتوافقية Cloudinary مع Django 5
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-
+# استخدام WhiteNoise لخدمة الملفات الثابتة بسرعة واستقرار
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ------------------------------------------------------------------
 # Media Files & Cloudinary Configurations
 # ------------------------------------------------------------------
